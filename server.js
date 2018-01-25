32f8324700c4a8117a9afba2c65d47016984bde6
loan <loancrea@gmail.com>
2018-01-25 21:42:08 +0100

    delete line models of old server

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const models = require('./server/models');
 var request = require('request');
 const path = require('path');
 const http = require('http');
 
-
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
